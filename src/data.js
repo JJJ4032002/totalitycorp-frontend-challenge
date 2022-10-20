4fd736d68d5535407532d6cdcc76b34ffe53cbf4
JJJ4032002 <rp4888888@gmail.com>
2022-10-20 20:01:19 +0530

    Add dummy data

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const LinksList = [
+  "Store",
+  "Mac",
+  "iPad",
+  "iPhone",
+  "Watch",
+  "AirPods",
+  "TV & Home",
+  "Only on Apple",
+  "Accessories",
+  "Support",
+];
+
+export default LinksList;
