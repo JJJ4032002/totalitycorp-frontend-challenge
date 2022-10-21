f545a54bfc1a660e4654bbe8cc1edda8e730f75c
JJJ4032002 <rp4888888@gmail.com>
2022-10-21 21:31:09 +0530

    Add Footer in App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import IpadPro from "./Components/iPadPro/IpadPro";
 import AppleTv from "./Components/AppleTv/AppleTv";
 import Gallery from "./Components/Gallery/Gallery";
 import Carousel from "./Components/Carousel/Carousel";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
       <AppleTv></AppleTv>
       <Gallery></Gallery>
       <Carousel></Carousel>
+      <Footer></Footer>
     </div>
   );
 }
