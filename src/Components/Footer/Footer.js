e5b6e016ac93cc62dc0be1f65d60c321d7e47242
JJJ4032002 <rp4888888@gmail.com>
2022-10-22 01:15:26 +0530

    Add CopyRight in Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,12 +2,14 @@ import React from "react";
 import AppleCardInfo from "./Components/AppleCardInfo/AppleCardInfo";
 import "./Footer.css";
 import Options from "./Components/Options/Options";
+import CopyRight from "./Components/CopyRight/CopyRight";
 function Footer() {
   return (
     <footer className="FooterWrapper">
       <AppleCardInfo></AppleCardInfo>
       <div className="footerDivider"></div>
       <Options></Options>
+      <CopyRight></CopyRight>
     </footer>
   );
 }
