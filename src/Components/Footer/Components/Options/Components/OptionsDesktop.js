5afa08c3de269146c54c751b1ec7a9c7eb6e3d31
JJJ4032002 <rp4888888@gmail.com>
2022-10-22 01:13:51 +0530

    Divide OptionsDesktop into multiple components

--- src/Components/Footer/Components/Options/Components/OptionsDesktop.js
+++ src/Components/Footer/Components/Options/Components/OptionsDesktop.js
@@ -1,32 +1,46 @@
 import React from "react";
 import { DesktopOptionList } from "../../../../../data";
+
+function Section({ Section }) {
+  return (
+    <div key={Section.Section} className="Section">
+      {Section.OptionLists.map((OptionBlockParam) => {
+        return (
+          <OptionBlock
+            key={OptionBlockParam.Heading}
+            OptionBlock={OptionBlockParam}
+          ></OptionBlock>
+        );
+      })}
+    </div>
+  );
+}
 function OptionsDesktop() {
   return (
     <>
-      {DesktopOptionList.map((Section) => {
+      {DesktopOptionList.map((SectionParam) => {
         return (
-          <div key={Section.Section} className="Section">
-            {Section.OptionLists.map((OptionBlock) => {
-              return (
-                <div key={OptionBlock.Heading} className="DesktopOptionsBlock">
-                  <p>{OptionBlock.Heading}</p>
-                  <ul className="OptionListWrapper Desktop">
-                    {OptionBlock.options.map((element) => {
-                      return (
-                        <li key={element} className="">
-                          {element}
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              );
-            })}
-          </div>
+          <Section key={SectionParam.Section} Section={SectionParam}></Section>
         );
       })}
     </>
   );
 }
 
+function OptionBlock({ OptionBlock }) {
+  return (
+    <div key={OptionBlock.Heading} className="DesktopOptionsBlock">
+      <p className="OptionsHeading">{OptionBlock.Heading}</p>
+      <ul className="OptionListWrapper Desktop">
+        {OptionBlock.options.map((element) => {
+          return (
+            <li key={element} className="DesktopOptionItem">
+              {element}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 export default OptionsDesktop;
