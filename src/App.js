5e7c62e225fa1f6032b834737f9e4300833add77
JJJ4032002 <rp4888888@gmail.com>
2022-10-21 17:12:05 +0530

    Add Carousel to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Ipad from "./Components/iPad/Ipad";
 import IpadPro from "./Components/iPadPro/IpadPro";
 import AppleTv from "./Components/AppleTv/AppleTv";
 import Gallery from "./Components/Gallery/Gallery";
+import Carousel from "./Components/Carousel/Carousel";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <IpadPro></IpadPro>
       <AppleTv></AppleTv>
       <Gallery></Gallery>
+      <Carousel></Carousel>
     </div>
   );
 }
